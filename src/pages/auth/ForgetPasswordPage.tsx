cf1b53a05305f09bbc83d9c02ee71c71cd75f85d
moon <ahmedabozaid.dev@gmail.com>
2026-01-22 12:18:39 +0200

    fixes side image for auth

--- src/pages/auth/ForgetPasswordPage.tsx
+++ src/pages/auth/ForgetPasswordPage.tsx
@@ -3,16 +3,18 @@ import ForgetPassForm from "@/components/auth/forget_password/ForgetPassForm"
 const ForgetPasswordPage = () => {
     return (
         <div className="flex h-full">
-        <div className="flex-1 flex items-center justify-center">
+        <div className="flex-1 flex items-center justify-center py-8">
             <ForgetPassForm />
         </div>
 
-        <div className="hidden lg:block lg:flex-1">
-            <img
-            src="/images/auth/auth_bg.jpg"
-            alt="bg"
-            className=" w-full object-cover"
-            />
+        <div className="hidden lg:block lg:flex-1 relative">
+            <div className="sticky top-0 h-screen">
+                <img
+                src="/images/auth/auth_bg.jpg"
+                alt="bg"
+                className="w-full h-full object-cover"
+                />
+            </div>
         </div>
         </div>
     )
--- src/pages/auth/NewPasswordPage.tsx
+++ src/pages/auth/NewPasswordPage.tsx
@@ -3,16 +3,18 @@ import NewPasswordFrom from "@/components/auth/new_password/NewPasswordFrom"
 const NewPasswordPage = () => {
     return (
         <div className="flex h-full">
-        <div className="flex-1 flex items-center justify-center">
+        <div className="flex-1 flex items-center justify-center py-8">
             <NewPasswordFrom />
         </div>
 
-        <div className="hidden lg:block lg:flex-1">
-            <img
-            src="/images/auth/auth_bg.jpg"
-            alt="bg"
-            className=" w-full object-cover"
-            />
+        <div className="hidden lg:block lg:flex-1 relative">
+            <div className="sticky top-0 h-screen">
+                <img
+                src="/images/auth/auth_bg.jpg"
+                alt="bg"
+                className="w-full h-full object-cover"
+                />
+            </div>
         </div>
         </div>
     )
