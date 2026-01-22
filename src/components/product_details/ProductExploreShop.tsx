46807ff233b36f4d3cd6bc7bd4e7eecab79eaf8a
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2026-01-22 11:23:11 +0200

    adds explore section

---
+++ src/components/product_details/ProductExploreShop.tsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom"
+
+const ProductExploreShop = () => {
+    return (
+        <section
+        className="relative my-17 md:h-136 h-67 bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: "url('/images/product/explore.jpg')" }}
+        >
+        <div className="absolute inset-0 bg-[#00000066] opacity-60" />
+
+        <div className="relative z-10 flex h-full flex-col items-center justify-end md:pb-10 pb-4">
+            <h2 className="mb-6 text-[#FEFEFE] md:text-[40px] text-base font-semibold text-center">
+            Timeless, Confident, Refined, Always Iconic.
+            </h2>
+
+            <Link to="/explore">
+            <div className="md:w-103.25 w-85.75 h-12 bg-[#FFFFFF33] rounded-4xl flex items-center justify-center text-[#FEFEFE] text-lg font-bold">
+                Explore Shop
+            </div>
+            </Link>
+        </div>
+        </section>
+    )
+}
+
+export default ProductExploreShop
--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -1,6 +1,7 @@
 import ComplateSelection from "@/components/product_details/ComplateSelection"
 import ProductDetailsHeader from "@/components/product_details/ProductDetailsHeader"
 import ProductDetialsData from "@/components/product_details/ProductDetialsData"
+import ProductExploreShop from "@/components/product_details/ProductExploreShop"
 import SeeStyle from "@/components/product_details/SeeStyle"
 
 const ProductDetailsPage = () => {
@@ -10,6 +11,7 @@ const ProductDetailsPage = () => {
             <ProductDetialsData />
             <ComplateSelection />
             <SeeStyle />
+            <ProductExploreShop />
         </div>
     )
 }
