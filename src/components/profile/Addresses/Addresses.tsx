b7c91da41f2452fe3b3cc583031d436456ea8dff
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2026-01-22 15:54:03 +0200

    adds address empty state

--- src/components/profile/Addresses/Addresses.tsx
+++ src/components/profile/Addresses/Addresses.tsx
@@ -1,7 +1,9 @@
 const Addresses = () => {
     return (
         <section>
-            
+            <h1 className="text-[#0B0B0B] text-[40px] font-semibold">
+                My Addresses
+            </h1>
         </section>
     )
 }
