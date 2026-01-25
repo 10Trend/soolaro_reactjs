15abf2b52049b2a19f881d4be7731e46afe63e05
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2026-01-25 13:35:29 +0200

    new password translation

--- src/components/auth/new_password/NewPasswordFrom.tsx
+++ src/components/auth/new_password/NewPasswordFrom.tsx
@@ -4,8 +4,10 @@ import { Eye } from "lucide-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import MobileBackHeader from "@/components/general/MobileBackHeader";
+import { useTranslation } from "react-i18next";
 
 const NewPasswordForm = () => {
+  const { t } = useTranslation("auth");
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
@@ -18,10 +20,10 @@ const NewPasswordForm = () => {
       <div className="md:mt-34 mt-8">
         <MobileBackHeader />
         <h2 className="text-[#0B0B0B] md:text-[40px] text-2xl font-bold md:font-semibold leading-[100%] text-center mx-auto mt-6">
-          Create New Password
+          {t("creating_new_password")}
         </h2>
         <p className="text-[#3B3B3B] text-base font-medium leading-[150%] mt-6 text-center">
-          Enter a new password for your account.
+          {t("enter_new_password_instruction")}
         </p>
 
         <div className="md:mt-14.5 mt-8 relative">
@@ -29,18 +31,18 @@ const NewPasswordForm = () => {
             htmlFor="password"
             className="text-[#0B0B0B] md:text-base text-sm font-semibold leading-[100%]"
           >
-            New Password
+            {t("new_password")}
           </label>
           <input
             type={showPassword ? "text" : "password"}
             name="password"
             className="w-full h-14 border border-[#DEDDDD] rounded-4xl mt-3 px-4"
-            placeholder="Enter your password"
+            placeholder={t("enter_your_password")}
           />
           <button
             type="button"
             onClick={() => setShowPassword((prev) => !prev)}
-            className="absolute top-13 right-4 cursor-pointer"
+            className="absolute top-13 ltr:right-4 rtl:left-4 cursor-pointer"
           >
             {showPassword ? <Eye /> : <ClosedEye />}
           </button>
@@ -51,26 +53,26 @@ const NewPasswordForm = () => {
             htmlFor="confirm_password"
             className="text-[#0B0B0B] md:text-base text-sm font-semibold leading-[100%]"
           >
-            Confirm New Password
+            {t("confirm_new_password")}
           </label>
           <input
             type={showConfirmPassword ? "text" : "password"}
-            name="password"
+            name="confirm_password"
             className="w-full h-14 border border-[#DEDDDD] rounded-4xl mt-3 px-4"
-            placeholder="Enter your password"
+            placeholder={t("enter_your_password")}
           />
           <button
             type="button"
             onClick={() => setShowConfirmPassword((prev) => !prev)}
-            className="absolute top-13 right-4 cursor-pointer"
+            className="absolute top-13 ltr:right-4 rtl:left-4 cursor-pointer"
           >
             {showConfirmPassword ? <Eye /> : <ClosedEye />}
           </button>
         </div>
 
-        <Link to="/otp">
+        <Link to="/signin">
           <button className="w-full h-14 bg-[#018884] rounded-4xl md:mt-10 mt-6 text-[#FEFEFE] text-base font-bold">
-            Save
+            {t("save")}
           </button>
         </Link>
       </div>
