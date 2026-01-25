58cf0cfc247002cee6db14d62ebdacc20fce8890
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2026-01-25 10:36:06 +0200

    change password - mobile view

--- src/components/profile/change_password/ChangePassword.tsx
+++ src/components/profile/change_password/ChangePassword.tsx
@@ -1,7 +1,9 @@
+import MobileBackHeader from "@/components/general/MobileBackHeader";
 import ClosedEye from "@/components/icons/auth/ClosedEye"
 import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Eye } from "lucide-react"
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const ChangePassword = () => {
     const [showOldPassword, setShowOldPassword] = useState(false);
@@ -9,14 +11,21 @@ const ChangePassword = () => {
     const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
     return (
-        <section>
-            <h1 className="text-[#0B0B0B] text-[40px] font-semibold">
+        <section className="mb-12">
+            <h1 className="text-[#0B0B0B] text-[40px] font-semibold md:block hidden">
                 Change Password
             </h1>
+            
+            <Link to='/' className="md:hidden flex items-center gap-3">
+                <MobileBackHeader />
+                <p className="text-[#0B0B0B] md:text-base text-sm font-semibold mb-6">
+                    Change Password
+                </p>
+            </Link>
 
-            <div className="mt-12">
+            <div className="md:mt-12 mt-4">
                 <div className="relative">
-                    <label htmlFor="old_password" className="text-[#0B0B0B] text-base font-semibold">
+                    <label htmlFor="old_password" className="text-[#0B0B0B] md:text-base text-sm font-semibold">
                         Old Password
                     </label>
                     <input
@@ -35,7 +44,7 @@ const ChangePassword = () => {
                 </div>
 
                 <div className="relative mt-6">
-                    <label htmlFor="new_password" className="text-[#0B0B0B] text-base font-semibold">
+                    <label htmlFor="new_password" className="text-[#0B0B0B] md:text-base text-sm font-semibold">
                         New Password
                     </label>
                     <input
@@ -54,7 +63,7 @@ const ChangePassword = () => {
                 </div>
 
                 <div className="relative mt-6">
-                    <label htmlFor="confirm_password" className="text-[#0B0B0B] text-base font-semibold">
+                    <label htmlFor="confirm_password" className="text-[#0B0B0B] md:text-base text-sm font-semibold">
                         Confirm New Password
                     </label>
                     <input
@@ -74,7 +83,7 @@ const ChangePassword = () => {
 
                 <Dialog>
                     <DialogTrigger className="w-full">
-                        <button className="w-full h-14 bg-[#018884] rounded-4xl mt-8 text-[#FEFEFE] text-lg font-bold">
+                        <button className="w-full h-14 bg-[#018884] rounded-4xl mt-8 text-[#FEFEFE] md:text-lg text-base font-bold">
                             Save Changes
                         </button>
                     </DialogTrigger>
