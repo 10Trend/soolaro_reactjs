e3721567a76c11873e538822f4699f56e6d6a4fb
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2026-01-25 12:53:51 +0200

    link category

--- src/components/home/ShopByCategory.tsx
+++ src/components/home/ShopByCategory.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 const ShopByCategory = () => {
   const { t } = useTranslation("home");
@@ -17,7 +18,8 @@ const ShopByCategory = () => {
 
       <div className="md:mt-12 mt-6">
         <div className="flex gap-8">
-          <motion.div
+          <Link to='/category' className="w-full">
+            <motion.div
             initial={{ opacity: 0, scale: 0.95 }}
             whileInView={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.8 }}
@@ -35,43 +37,49 @@ const ShopByCategory = () => {
               {t("best_seller")}
             </h2>
           </motion.div>
+          </Link>
+
+          <Link to='/category' className="w-full">
+            <motion.div
+              initial={{ opacity: 0, scale: 0.95 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.8, delay: 0.1 }}
+              viewport={{ once: true }}
+              className="relative overflow-hidden rounded-4xl group flex-[2]"
+            >
+              <motion.img
+                whileHover={{ scale: 1.05 }}
+                transition={{ duration: 0.6 }}
+                src="/images/home/new.jpg"
+                alt={t("new_arrival")}
+                className="w-full md:h-107 h-35.25 bg-[#0000001A] opacity-70 object-cover"
+              />
+              <h2 className="text-[#0B0B0B] md:text-[32px] text-sm font-semibold leading-[100%] absolute md:bottom-6 bottom-3 md:left-6 left-2">
+                {t("new_arrival")}
+              </h2>
+            </motion.div>
+          </Link>
+        </div>
+          <Link to='/category'>
           <motion.div
-            initial={{ opacity: 0, scale: 0.95 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.8, delay: 0.1 }}
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
             viewport={{ once: true }}
-            className="relative overflow-hidden rounded-4xl group flex-[2]"
+            className="relative overflow-hidden rounded-4xl md:mt-10 mt-4 group"
           >
             <motion.img
               whileHover={{ scale: 1.05 }}
               transition={{ duration: 0.6 }}
-              src="/images/home/new.jpg"
-              alt={t("new_arrival")}
-              className="w-full md:h-107 h-35.25 bg-[#0000001A] opacity-70 object-cover"
+              src="/images/home/summer.jpg"
+              alt={t("summer_collection")}
+              className="w-full md:h-107 h-39.5 bg-[#0000001A] opacity-70 object-cover"
             />
-            <h2 className="text-[#0B0B0B] md:text-[32px] text-sm font-semibold leading-[100%] absolute md:bottom-6 bottom-3 md:left-6 left-2">
-              {t("new_arrival")}
+            <h2 className="text-[#0B0B0B] md:text-[32px] text-sm font-semibold leading-[100%] absolute md:top-6 top-3 md:left-6 left-2">
+              {t("summer_collection")}
             </h2>
           </motion.div>
-        </div>
-        <motion.div
-          initial={{ opacity: 0, y: 30 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, delay: 0.2 }}
-          viewport={{ once: true }}
-          className="relative overflow-hidden rounded-4xl md:mt-10 mt-4 group"
-        >
-          <motion.img
-            whileHover={{ scale: 1.05 }}
-            transition={{ duration: 0.6 }}
-            src="/images/home/summer.jpg"
-            alt={t("summer_collection")}
-            className="w-full md:h-107 h-39.5 bg-[#0000001A] opacity-70 object-cover"
-          />
-          <h2 className="text-[#0B0B0B] md:text-[32px] text-sm font-semibold leading-[100%] absolute md:top-6 top-3 md:left-6 left-2">
-            {t("summer_collection")}
-          </h2>
-        </motion.div>
+          </Link>
       </div>
     </section>
   );
