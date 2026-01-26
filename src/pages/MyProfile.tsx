87dce6800ad4dd329dde24498053509b9c074eda
moon <ahmedabozaid.dev@gmail.com>
2026-01-26 11:19:27 +0200

    fixes initial

--- src/pages/MyProfile.tsx
+++ src/pages/MyProfile.tsx
@@ -1,20 +1,19 @@
-import ProfileMobileView from "@/components/profile/ProfileMobileView"
-import ProfileSidebar from "@/components/profile/ProfileSidebar"
-import { Outlet, useLocation } from "react-router-dom"
+import ProfileMobileView from "@/components/profile/ProfileMobileView";
+import ProfileSidebar from "@/components/profile/ProfileSidebar";
+import { Outlet, useLocation, Navigate } from "react-router-dom";
 
 const ProfilePage = () => {
-  const location = useLocation()
-  const isProfileRoot = location.pathname === "/profile"
+  const location = useLocation();
+  const isProfileRoot = location.pathname === "/profile";
+
+  if (isProfileRoot) {
+    return <Navigate to="/profile/info" replace />;
+  }
 
   return (
     <section className="container mx-auto md:py-10">
       <div className="grid grid-cols-12 gap-8">
-
-        <aside
-          className={`col-span-12 md:col-span-3 ${
-            !isProfileRoot ? "hidden md:block" : ""
-          }`}
-        >
+        <aside className={`col-span-12 md:col-span-3 hidden md:block`}>
           <div className="md:block hidden">
             <ProfileSidebar />
           </div>
@@ -24,17 +23,12 @@ const ProfilePage = () => {
           </div>
         </aside>
 
-        <main
-          className={`col-span-12 md:col-span-9 ${
-            isProfileRoot ? "hidden md:block" : ""
-          }`}
-        >
+        <main className={`col-span-12 md:col-span-9`}>
           <Outlet />
         </main>
-
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
