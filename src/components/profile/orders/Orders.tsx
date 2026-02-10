92a67eabeb5b6bf29f6c74696c1728ef4e7887e8
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2026-02-10 12:17:41 +0200

    fixes last orders direction

--- src/components/profile/orders/Orders.tsx
+++ src/components/profile/orders/Orders.tsx
@@ -189,7 +189,7 @@ const Orders = () => {
             )}
           </TabsContent>
 
-          <TabsContent value="last">
+          <TabsContent value="last" dir={i18n.language === "ar" ? "rtl" : "ltr"}>
             {loading ? (
               <div className="flex items-center justify-center py-12">
                 <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-[#018884]"></div>
