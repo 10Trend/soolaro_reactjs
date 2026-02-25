821a3205e3c3e5b6f6683df7ba587fa266fdf4de
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2026-02-25 13:33:06 +0200

    adds delivered date

--- src/components/profile/orders/order_details/OrderSummary.tsx
+++ src/components/profile/orders/order_details/OrderSummary.tsx
@@ -5,6 +5,7 @@ const OrderSummary = () => {
                 Item Summary
             </h2>
 
+            {/* Item summary */}
             <div className="md:mt-8 mt-6 p-3 border border-[#DEDDDD] rounded-[20px] flex gap-4">
                 <div className="md:w-[126px] w-[86px] md:h-[126px] h-[86px] bg-[#F6F6F6] rounded-[8px]">
                     <img
@@ -40,6 +41,16 @@ const OrderSummary = () => {
                     </div>
 
                 </div>
+            </div>
+
+            {/* Delivered */}
+            <div className="md:mt-8 mt-6 p-3 border border-[#DEDDDD] rounded-[20px]">
+                <p className="text-[#0B0B0B] md:text-lg text-xs font-medium">
+                    Delivered
+                </p>
+                <h3 className="text-[#0B0B0B] md:text-xl text-sm font-semibold mt-1">
+                    on Sun, 3 Jan,2025
+                </h3>
             </div>
         </section>
     )
